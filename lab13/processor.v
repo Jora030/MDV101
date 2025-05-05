8a73d84c0af7ed5be88a0c1f1c631ec2a1e7863c
Zhassulan <muratbekov.z@mail.ru>
2025-05-05 17:05:19 +0500

    Update processor.v

--- lab13/processor.v
+++ lab13/processor.v
@@ -2,14 +2,14 @@ module processor (
     input clk
 );
 
-reg [15:0] memory [0:31]; // Память инструкций на 32 слова
+reg [15:0] memory [0:31];
 reg [7:0] pc;
 wire [15:0] r1, r2, r3;
 
 datapath dp (.clk(clk), .instruction(memory[pc]), .r1(r1), .r2(r2), .r3(r3));
 
 initial begin
-    $readmemb("program.mem", memory); // Загружаем программу
+    $readmemb("program.mem", memory);
     pc = 0;
 end
 
