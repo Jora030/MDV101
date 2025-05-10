f3d74beca46f3eee7231bce39aa3803c6b5fa504
Zhassulan <muratbekov.z@mail.ru>
2025-05-10 10:51:32 +0500

    Delete 16 directory

--- 16/obj_dir/cosim.d
+++
@@ -1,6 +0,0 @@
-cosim.o: ../cosim.cpp Vtop.h \
- C:/msys64/mingw64/share/verilator/include/verilated.h \
- C:/msys64/mingw64/share/verilator/include/verilatedos.h \
- C:/msys64/mingw64/share/verilator/include/verilated_config.h \
- C:/msys64/mingw64/share/verilator/include/verilated_types.h \
- C:/msys64/mingw64/share/verilator/include/verilated_funcs.h
